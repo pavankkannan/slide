02a13b02ed7eadb1a85b59a5defe92f2da799438
Shane <ssoms2@uic.edu>
2025-02-08 04:10:52 -0600

    Spot page

--- src/pages/Spot.jsx
+++ src/pages/Spot.jsx
@@ -1,5 +1,27 @@
+
+const bg = () => {
+    return (
+        <div
+          style={{
+            backgroundImage: "url('/src/.jpg')", // If in 'public' folder
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            height: "100vh",
+            width: "100vw",
+          }}
+        >
+          <h1 style={{ color: "white", textAlign: "center", paddingTop: "20vh" }}>
+            Welcome to My Page
+          </h1>
+        </div>
+      );
+}
+
+
 const Spot = () => {
     return <h1>Spot Page</h1>;
   };
   
   export default Spot;
+
