8cf4aa39e2ebc9293e932f54234c344d08126a4b
Shane <ssoms2@uic.edu>
2025-02-08 05:12:13 -0600

    spot.jsx update

--- src/pages/Spot.jsx
+++ src/pages/Spot.jsx
@@ -27,6 +27,7 @@ const Spot = () => {
             <NavBar/>
             <h1>Spot Page</h1>;
             <input type="button" value= "Reviews" />
+            <Text>Hello World</Text>
             
         </>
     )
